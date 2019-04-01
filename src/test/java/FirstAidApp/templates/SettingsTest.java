52c9aea981f41a7253f9282c63179f884af9ff32
TLDRQwerty <charliecarolan1999@gmail.com>
2019-04-01 01:06:56 +0000

    Added settings testing class. Having errors creating new settings class. Need to have a further look into

---
+++ src/test/java/FirstAidApp/templates/SettingsTest.java
@@ -0,0 +1,21 @@
+package FirstAidApp.templates;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SettingsTest {
+    // TODO: Don't know how to fix enums with testing. Gives cannot access non-static method in static call.
+
+    @Test
+    public void getColorBlind() {
+    }
+
+    @Test
+    public void getMagnifyText() {
+    }
+
+    @Test
+    public void getCaptionImages() {
+    }
+}
